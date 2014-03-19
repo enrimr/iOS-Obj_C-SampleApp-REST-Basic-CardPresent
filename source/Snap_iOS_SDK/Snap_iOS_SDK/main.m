9795465c4ca2ee8bc2e2dd30de08ac3a93336b52
markmalinowski <mark.malinowski@evopayments.com>
2014-03-19 14:32:03 -0600

    Initial Repo Creation

---
+++ source/Snap_iOS_SDK/Snap_iOS_SDK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Snap_iOS_SDK
+//
+//  Created by Darren Adelgren on 12/30/13.
+//  Copyright (c) 2013 Darren Adelgren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
